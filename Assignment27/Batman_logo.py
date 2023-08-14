6aa7b6234bb4ce1f41d080a6e63285eeeb80ecd8
Mahdi Taheri <2021roro14@gmail.com>
2023-08-14 15:39:47 +0330

    Update Batman_logo.py

--- Assignment27/Batman_logo.py
+++ Assignment27/Batman_logo.py
@@ -4,8 +4,8 @@ import numpy as np
 img = cv2.imread("Assignment27/bat.jpg")
 
 img = cv2.cvtColor(img,cv2. COLOR_RGB2GRAY)
-gg,img = cv2.threshold(img,220,255, cv2.THRESH_BINARY)
-cv2.putText(img,"Batman",(60,190), fontFace=cv2.FONT_HERSHEY_SCRIPT_COMPLEX,fontScale= 1, color=(0,0,0))
+gg,img = cv2.threshold(img,210,255, cv2.THRESH_BINARY_INV)
+cv2.putText(img,"Batman",(60,190), fontFace=cv2.FONT_HERSHEY_SCRIPT_COMPLEX,fontScale= 1, color=(255,255,255))
 
 cv2.imshow("result",img)
 cv2.waitKey()

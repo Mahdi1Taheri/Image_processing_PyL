dcde5c319b8fde36fc1ade70c0f648c7f8820193
Mahdi Taheri <2021roro14@gmail.com>
2023-11-10 18:17:33 +0330

    Update black_hole.py

--- Assignment29/black_hole.py
+++ Assignment29/black_hole.py
@@ -2,15 +2,15 @@ import cv2
 import numpy as np
 import os
 
+lst = []
+count = 1
 while True:
-    count = 1
     images_path = os.listdir(f'inputs/galaxy/{str(count)}')
 
     images = []
 
     for image_path in images_path:
         image = cv2.imread(f'inputs/galaxy/{str(count)}/' + image_path)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         image = image.astype(np.float32)
         images.append(image)
 
@@ -22,7 +22,20 @@ while True:
     result = result / len(images)
     result = result.astype(np.uint8)
 
-    cv2.imwrite(f"output/result{str(count)}.jpg", result)
+    if os.path.isdir('output/galaxy'):
+        pass
+    else: 
+        os.mkdir("output/galaxy")
+    cv2.imwrite(f"output/galaxy/result{str(count)}.jpg", result)
     count += 1
     if count == 5:
+        galaxy_imgs = os.listdir('output/galaxy/')
+        for i in galaxy_imgs:
+            read_imgs = cv2.imread("output/galaxy/" + i)
+            lst.append(read_imgs)
+        all0 = np.concatenate((lst[0],lst[2]),0)
+        all1 = np.concatenate((lst[1],lst[3]),0)
+        all = np.concatenate((all0,all1),1)
+        
+        cv2.imwrite("output/galaxy/final.jpg",all)
         break

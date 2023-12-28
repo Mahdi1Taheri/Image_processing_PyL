609737051a2b37e41238f659145ca99243178325
Mahdi Taheri <2021roro14@gmail.com>
2023-12-29 00:17:51 +0330

    Update foreground_focus.py

--- Assignment31/foreground_focus.py
+++ Assignment31/foreground_focus.py
@@ -3,4 +3,14 @@ import cv2
 
 img = cv2.imread('input/flower_input.jpg',cv2.IMREAD_GRAYSCALE)
 
-mask =
+_,thresh1 = cv2.threshold(img,150,255,cv2.THRESH_BINARY_INV)
+thresh = 255 - thresh1
+
+blur = cv2.blur(img,(25,25),0)
+
+res1 = cv2.bitwise_and(thresh,img)
+bg = cv2.subtract(blur,res1)
+res2 = cv2.bitwise_and(bg,thresh1)
+final = cv2.add(res1,res2)
+
+cv2.imwrite('output/foreground_focus.png',final)
